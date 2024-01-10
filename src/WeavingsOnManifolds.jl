18a7b53728688afe56dc7109b70104fac438a6f1
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2024-01-10 11:20:07 +0100

    update

--- src/WeavingsOnManifolds.jl
+++ src/WeavingsOnManifolds.jl
@@ -6,9 +6,10 @@ mutable struct WeavingOnManifold
     constraints
     energyFunction
     lagrangeSystem
-    localVariables
+    coordinateVariables
+    lagrangeMultipliers
 
-    function WeavingOnManifold(offsetList::Vector{Bool}, barConnections::Vector, cableConnections::Vector, ambientVariables::Vector{Variable}, manifold::String, offset::Float64)
+    function WeavingOnManifold(offsetList::Vector{Bool}, bars::Vector, cables::Vector, ambientVariables::Vector{Variable}, manifold::String, offset::Float64)
         (offset<0.5 && offset>0.05) || warn("We recommend an offset between 5% and 50%.")
         @var xs[1:3, 1:length(offsetList)]
         manifoldEquations, barEquations = [], []
@@ -17,8 +18,10 @@ mutable struct WeavingOnManifold
         else
             throw(error("Only the Sphere is implemented right now!"))
         end
-        barEquations = [sum( (xs[:,bar[1]] - xs[:,bar[2]]).^2 ) - (2*offset)^2 for bar in barConnections]
-        new(vcat(manifoldEquations, barEquations), )
+        barEquations = [sum( (xs[:,bar[1]] - xs[:,bar[2]]).^2 ) - (2*offset)^2 for bar in bars]
+        energyFunction = sum([sum((xs[:,cable[1]] - xs[:,cable[2]]).^2) for cable in cables])
+        @var λ[1:(length(manifoldEquations)+length(barEquations))]
+        new(vcat(manifoldEquations, barEquations), energyFunction, energyFunction + λ'*vcat(manifoldEquations, barEquations), reduce(vcat, xs), λ)
     end
 end
 
