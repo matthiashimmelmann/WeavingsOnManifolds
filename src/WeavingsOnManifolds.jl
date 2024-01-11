7c0e2ddacf675c14402d428573c902812207c84f
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2024-01-11 21:42:19 +0100

    update

--- src/WeavingsOnManifolds.jl
+++ src/WeavingsOnManifolds.jl
@@ -1,5 +1,5 @@
 module WeavingsOnManifolds
-import HomotopyContinuation: Expression, Variable, @var, evaluate, differentiate
+import HomotopyContinuation: Expression, Variable, @var, evaluate, differentiate, System, real_solutions, solve
 import LinearAlgebra: pinv, norm, cross
 import Implicit3DPlotting.GLMakiePlottingLibrary: Figure, Axis3, hidespines!, hidedecorations!, Point3f0, scatter!, linesegments!, RGBA
 import Implicit3DPlotting: plot_implicit_surface!
@@ -17,13 +17,15 @@ struct WeavingOnManifold
     cables
     planes
     variablePositions
+    samples
 
-    function WeavingOnManifold(offsetList::Vector{Bool}, bars::Vector, cables::Vector, planes::Vector, manifold::String, offset::Float64)
+    function WeavingOnManifold(offsetList::Vector{Bool}, bars::Vector, cables::Vector, planes::Vector, offset::Float64; manifold="sphere", implicitManifold=x->x[1]^2+x[2]^2+x[3]^2-1)
         (offset<=0.5 && offset>=0.05) || @warn "We recommend an offset between 5% and 50%."
+        implicitManifold([1,2,3])
         @var x[1:3, 1:length(offsetList)]
 
         xs = zeros(Expression,(3,length(offsetList)))
-        manifoldEquations, barEquations, xvarz, positions = Vector{Expression}([]), Vector{Expression}([]), Vector{Variable}([]), []
+        manifoldEquations, barEquations, xvarz, positions, samples = Vector{Expression}([]), Vector{Expression}([]), Vector{Variable}([]), [], []
         if isequal(lowercase(manifold), "sphere")
             xs[:,1] = [offsetList[1] ? 1+offset : 1-offset, 0, 0]
             xs[:,2] = [0, x[2,2], x[3,2]]
@@ -44,13 +46,31 @@ struct WeavingOnManifold
             end
 
             manifoldEquations = [offsetList[i] ? sum( xs[:,i].^2 ) - (1+offset)^2 : sum( xs[:,i].^2 ) - (1-offset)^2 for i in 1:length(offsetList)]
+        
+        elseif isequal(lowercase(manifold), "free")
+            @var y[1:3]
+            F = System([implicitManifold(y)+offset*2*(offsetList[1]-0.5), rand(Float64,3)'*y, rand(Float64,3)'*y])
+            sols = real_solutions(solve(F))
+            
+            xs[:,1] = sols[1]
+            xs[:,2] = [sols[2][1], x[2,2], x[3,2]]
+            for i in 3:length(offsetList)
+                xs[:,i] = x[:,i]
+            end
+
+            append!(barEquations, [sum((xs[:,bar[1]]-xs[:,bar[2]]).^2)-(2*offset)^2 for bar in bars])
+            append!(samples, [sols[1],sols[2]])
+            append!(xvarz, x[2:3,2]); append!(positions, [(2,2),(3,2)])
+            foreach(i->append!(xvarz, x[:,i]), 3:length(offsetList)); foreach(i->append!(positions, [(1,i),(2,i),(3,i)]), 3:length(offsetList))
+            manifoldEquations = [implicitManifold(xs[:,i])+offset*2*(offsetList[i]-0.5) for i in 2:length(offsetList)]
+
         else
             throw(error("Only weavings on the sphere are implemented right now!"))
         end
         #barEquations = [sum( (xs[:,bar[1]] - xs[:,bar[2]]).^2 ) - (2*offset)^2 for bar in bars]
         planeEquations = vcat([[cross(xs[:,plane[2]]-xs[:,plane[1]], xs[:,plane[3]]-xs[:,plane[2]])'*(xs[:,plane[4]]-xs[:,plane[3]]), cross(xs[:,plane[2]]-xs[:,plane[1]], xs[:,plane[3]]-xs[:,plane[2]])'*(xs[:,plane[1]]-xs[:,plane[4]])] for plane in planes]...)
         energyFunction = sum([sum((xs[:,cable[1]] - xs[:,cable[2]]).^2) for cable in cables])
-        new(lowercase(manifold), offsetList, offset, Vector{Expression}(vcat(manifoldEquations, planeEquations)), xvarz, bars, cables, planes, positions)
+        new(lowercase(manifold), offsetList, offset, Vector{Expression}(vcat(manifoldEquations, planeEquations)), xvarz, bars, cables, planes, positions, samples)
     end
 end
 
@@ -60,6 +80,10 @@ function toMatrix(configuration, Weave::WeavingOnManifold)
         global count = 1
         p0[:,1] = [Weave.offsetList[1] ? 1+Weave.offset : 1-Weave.offset, 0, 0]
         p0[1,2] = 0
+    elseif isequal(Weave.manifold, "free")
+        global count = 1
+        p0[:,1] = Weave.samples[1]
+        p0[1,2] = Weave.samples[2][1]
     else
         throw(error("Only weavings on the sphere are implemented right now!"))
     end
@@ -106,9 +130,8 @@ function plotWeaving(configuration::Vector{Float64}, Weave::WeavingOnManifold)
 end
 
 function test()
-    Weave = WeavingsOnManifolds.WeavingOnManifold([false,true,false,true, true,false,true,false, false,true,false,true], [(1,5),(2,11),(3,7),(4,9),(6,10),(8,12)], [(1,2),(2,3),(3,4),(4,1), (5,6),(6,7),(7,8),(8,5), (9,10),(10,11),(11,12),(12,9)], [(1,2,3,4), (5,6,7,8), (9,10,11,12)], "sphere",0.1)
+    Weave = WeavingsOnManifolds.WeavingOnManifold([false,true,false,true, true,false,true,false, false,true,false,true], [(1,5),(2,11),(3,7),(4,9),(6,10),(8,12)], [(1,2),(2,3),(3,4),(4,1), (5,6),(6,7),(7,8),(8,5), (9,10),(10,11),(11,12),(12,9)], [(1,2,3,4), (5,6,7,8), (9,10,11,12)],0.1)
     p0 = [1 0 0; 0 -1 0; -1 0 0; 0 1 0; 1 0 0; 0 0 -1; -1 0 0; 0 0 1; 0 1 0; 0 0 -1; 0 -1 0; 0 0 1]'
-    display(Weave)
     initialConfiguration = toArray(p0, Weave)
     q = newtonCorrect(initialConfiguration, Weave.coordinateVariables, Weave.constraints; tol = 1e-8)
     plotWeaving(q, Weave)
